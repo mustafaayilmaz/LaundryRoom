2611ff721bef0a349193b3395da5d955fb5a31b0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-02-15 00:50:24 +0300

    talep oluşturma modalini güncelle

--- src/pages/Queue.jsx
+++ src/pages/Queue.jsx
@@ -1,9 +1,49 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonList, IonModal, IonSelect, IonSelectOption, IonTitle, IonToggle, IonToolbar } from '@ionic/react'
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import Authorized from '../layouts/Authorized'
+import { kurutmaProgramları, sıcaklıklar, yıkamaProgramları } from '../types/programlar'
+import { ücret } from '../types/ücret'
 
 export const Queue = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	const [isKurutma, setIsKurutma] = useState(false)
+	const [isYumuşatıcı, setIsYumuşatıcı] = useState(false)
+	const [isDeterjan, setIsDeterjan] = useState(false)
+	const [toplamÜcret, setToplamÜcret] = useState(ücret.taban)
+
+	const toggleKurutma = () => {
+		setIsKurutma(!isKurutma)
+		if (!isKurutma) {
+			setToplamÜcret(toplamÜcret + ücret.kurutma)
+		} else {
+			setToplamÜcret(toplamÜcret - ücret.kurutma)
+		}
+	}
+	const toggleYumuşatıcı = () => {
+		setIsYumuşatıcı(!isYumuşatıcı)
+		if (!isYumuşatıcı) {
+			setToplamÜcret(toplamÜcret + ücret.yumuşatıcı)
+		} else {
+			setToplamÜcret(toplamÜcret - ücret.yumuşatıcı)
+		}
+	}
+	const toggleDeterjan = () => {
+		setIsDeterjan(!isDeterjan)
+		if (!isDeterjan) {
+			setToplamÜcret(toplamÜcret + ücret.deterjan)
+		} else {
+			setToplamÜcret(toplamÜcret - ücret.deterjan)
+		}
+	}
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm()
+
+	const onSubmit = data => console.log(data)
 	return (
 		<Authorized>
 			<IonContent className="ion-padding">
@@ -14,49 +54,53 @@ export const Queue = () => {
 					<IonHeader>
 						<IonToolbar>
 							<IonTitle>Çamaşır Talebi Oluşturma</IonTitle>
-							<IonButtons slot="end">
+							<IonButtons slot="start">
 								<IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
 							</IonButtons>
 						</IonToolbar>
 					</IonHeader>
 					<IonContent className="ion-padding">
-						<IonList>
-							<IonItem>
-								<IonSelect label="Yıkama Programı" labelPlacement="floating">
-									<IonSelectOption value="Pamuklu">Pamuklu</IonSelectOption>
-									<IonSelectOption value="Sentetik">Sentetik</IonSelectOption>
-									<IonSelectOption value="Mix">Mix</IonSelectOption>
-								</IonSelect>
-							</IonItem>
-							<IonItem>
-								<IonSelect label="Yıkama Sıcaklığı" labelPlacement="floating">
-									<IonSelectOption value="1">30°</IonSelectOption>
-									<IonSelectOption value="2">40°</IonSelectOption>
-									<IonSelectOption value="3">60°</IonSelectOption>
-								</IonSelect>
-							</IonItem>
-							<IonItem>
-								<IonToggle>Kurutma</IonToggle>
-							</IonItem>
-							<IonItem>
-								<IonSelect label="Kurutma Programı" labelPlacement="floating">
-									<IonSelectOption value="pDolap">Pamuklu Dolap Kuruluğu</IonSelectOption>
-									<IonSelectOption value="sDolap">Sentetik Dolap Kuruluğu</IonSelectOption>
-									<IonSelectOption value="spor">Spor</IonSelectOption>
-									<IonSelectOption value="mix">Karışık</IonSelectOption>
-								</IonSelect>
-							</IonItem>
-							<IonItem>
-								<IonToggle>Şahsi Deterjan</IonToggle>
-							</IonItem>
-							<IonItem>
-								<IonInput label="Ek Talep" labelPlacement="floating" placeholder="Ek Taleplerinizi Yazabilirsiniz"></IonInput>
-							</IonItem>
-							<IonItem>
-								<h1>Toplam Fiyat 5₺</h1>
-							</IonItem>
-						</IonList>
-						<IonButton>Çamaşır Talebini Yolla</IonButton>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<IonList>
+								<IonItem>
+									<IonSelect label="Yıkama Programı" labelPlacement="floating" onIonChange={e => console.log(yıkamaProgramları[e.detail.value])} {...register('yıkamaProgramı')}>
+										{Object.values(yıkamaProgramları).map(program => (
+											<IonSelectOption value={program.value}>{program.label}</IonSelectOption>
+										))}
+									</IonSelect>
+								</IonItem>
+								<IonItem>
+									<IonSelect label="Yıkama Sıcaklığı" labelPlacement="floating" onIonChange={e => console.log(e.detail.value)}>
+										{sıcaklıklar.map(sıcaklık => (
+											<IonSelectOption value={sıcaklık.value}>{sıcaklık.label}</IonSelectOption>
+										))}
+									</IonSelect>
+								</IonItem>
+								<IonItem>
+									<IonToggle onIonChange={toggleKurutma}>Kurutma</IonToggle>
+								</IonItem>
+								<IonItem>
+									<IonSelect label="Kurutma Programı" labelPlacement="floating" disabled={!isKurutma} onIonChange={e => console.log(kurutmaProgramları[e.detail.value])}>
+										{Object.values(kurutmaProgramları).map(program => (
+											<IonSelectOption value={program.value}>{program.label}</IonSelectOption>
+										))}
+									</IonSelect>
+								</IonItem>
+								<IonItem>
+									<IonToggle onIonChange={toggleDeterjan}>Şahsi Deterjan</IonToggle>
+								</IonItem>
+								<IonItem>
+									<IonToggle onIonChange={toggleYumuşatıcı}>Şahsi Yumuşatıcı</IonToggle>
+								</IonItem>
+								<IonItem>
+									<IonInput label="Ek Talep" labelPlacement="floating" placeholder="Ek Taleplerinizi Yazabilirsiniz"></IonInput>
+								</IonItem>
+								<IonItem>
+									<h3>Toplam Fiyat {toplamÜcret}₺</h3>
+								</IonItem>
+							</IonList>
+							<IonButton type="submit">Çamaşır Talebini Yolla</IonButton>
+						</form>
 					</IonContent>
 				</IonModal>
 			</IonContent>
---
+++ src/types/ücret.js
@@ -0,0 +1,6 @@
+export const ücret = {
+	taban: 3,
+	kurutma: 2,
+	deterjan: 2,
+	yumuşatıcı: 2
+}
