125b3e7e243a8d694a0dcc764c369383e7de7a93
mustafaayilmaz <myilmaz.my3825@gmail.com>
2024-02-26 12:23:13 +0300

    bakiye tanımlama kısmı oluşturuldu

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,12 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react'
-import React from 'react'
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonHeader, IonInput, IonItem, IonModal, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react'
+
+import React, { useState } from 'react'
 import { useSignOut } from 'react-firebase-hooks/auth'
 import Authorized from '../layouts/Authorized'
 import firebaseClient from '../lib/firebase/firebase'
 export const Profile = () => {
 	const [signOut, loading, error] = useSignOut(firebaseClient.auth)
-
+	const [isOpen, setIsOpen] = useState(false)
 	return (
 		<Authorized>
 			<IonCard className="ion-card">
@@ -14,6 +15,65 @@ export const Profile = () => {
 					<IonCardTitle>MUSTAFA YILMAZ</IonCardTitle>
 				</IonCardHeader>
 			</IonCard>
+			<IonButton onClick={() => setIsOpen(true)}>Bakiye Tanımla</IonButton>
+			<IonModal isOpen={isOpen}>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>Bakiye Tanımlama</IonTitle>
+						<IonButtons slot="end">
+							<IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+						</IonButtons>
+					</IonToolbar>
+					<IonItem>
+						<IonSelect label="Kullanıcı no seçiniz" placeholder="Kullanıcı No">
+							<IonSelectOption value="1">1</IonSelectOption>
+							<IonSelectOption value="2">2</IonSelectOption>
+							<IonSelectOption value="3">3</IonSelectOption>
+						</IonSelect>
+					</IonItem>
+					<IonItem>
+						<IonGrid>
+							<IonRow>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										10₺
+									</IonButton>
+								</IonCol>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										20₺
+									</IonButton>
+								</IonCol>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										25₺
+									</IonButton>
+								</IonCol>
+							</IonRow>
+							<IonRow>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										50₺
+									</IonButton>
+								</IonCol>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										100₺
+									</IonButton>
+								</IonCol>
+								<IonCol class="bakiye-col">
+									<IonButton class="bakiye-buton" expand="block">
+										<IonInput></IonInput>₺
+									</IonButton>
+								</IonCol>
+							</IonRow>
+						</IonGrid>
+					</IonItem>
+				</IonHeader>
+				<IonButton expand="block" onClick={() => setIsOpen(false)}>
+					Değişiklikleri Onayla
+				</IonButton>
+			</IonModal>
 			<IonButton
 				onClick={async () => {
 					const success = await signOut()
